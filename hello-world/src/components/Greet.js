f668dee5647d742e30d61550d003ac6f38b67088
landscape1250 <landscape.chen@gmail.com>
2019-08-25 18:28:07 -0700

    Update greet component.

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>Hello first project.</h1>
-}
+// function Greet(){
+//     return <h1>Hello first project.</h1>
+// }
 
+const Greet = () => <h1>Hello first project.</h1>
 
 export default Greet;
