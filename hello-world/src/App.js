1da4a52126353a4594bf55587a05607d5361b084
landscape1250 <landscape.chen@gmail.com>
2019-08-25 18:33:57 -0700

    Add welcome component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,13 +2,14 @@ import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet.js';
-
+import Welcome from './components/Welcome.js';
 
 class App extends Component {
    render(){
      return (
        <div className="App">
        <Greet />
+       <Welcome />
        </div>
      ); 
    }
