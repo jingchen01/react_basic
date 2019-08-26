383fd30789b96b2367f4c4a3a3fb85f76d0a5114
landscape1250 <landscape.chen@gmail.com>
2019-08-25 19:11:15 -0700

    Create hello component with JSX.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet.js';
 import Welcome from './components/Welcome.js';
+import Hello from './components/Hello.js';
 
 class App extends Component {
    render(){
      return (
        <div className="App">
-       <Greet />
-       <Welcome />
+       {/* <Greet /> }
+       {<Welcome /> */}
+       <Hello />
        </div>
      ); 
    }
---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// write component with JSX
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello ReactJS.</h1>
+        </div>
+    );
+}
+
+export default Hello;
