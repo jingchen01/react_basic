beeeb07896b7dfee278c5a8aeca05f0674f2b201
landscape1250 <landscape.chen@gmail.com>
2019-09-04 06:20:08 -0700

    Add button, onClick event

--- hello-world/src/components/Message.js
+++ hello-world/src/components/Message.js
@@ -2,21 +2,29 @@ import React, {Component} from 'react';
 
 
 class Message extends Component {
-constructor(){
-    super();
-    this.state = {
-        message : 'Welcome student01.'
+    constructor(){
+        super();
+        this.state = {
+            message : 'Welcome visitor.'
+        }
     }
-}
+
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for subscribing.'
+        });
+    }
 
     render () {
         return (
-            <h1>
-                {this.state.message}
-            </h1>
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={() => {this.changeMessage()}}>
+                    Subscribe
+                </button>
+            </div>
         );
     }
 }
-
 
 export default Message;
